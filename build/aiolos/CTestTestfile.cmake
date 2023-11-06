fffd5d9f7f95500354578f890706d90d56ab6746
BletillaQi <1370670703@qq.com>
2023-11-06 21:54:54 +0800

    refactor: 删除二进制文件

--- build/aiolos/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bletilla/Code/sim/src/aiolos
-# Build directory: /home/bletilla/Code/sim/build/aiolos
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/building/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bletilla/Code/sim/src/building
-# Build directory: /home/bletilla/Code/sim/build/building
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
